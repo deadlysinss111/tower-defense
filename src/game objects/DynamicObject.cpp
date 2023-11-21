9c354b55f956c753951e6b0cd59839a63ffda7b5
unknown <bboismenu@gamingcampus.local>
2023-11-21 12:05:49 +0100

    Save of pre-GameManager

---
+++ src/game objects/DynamicObject.cpp
@@ -0,0 +1,18 @@
+#include "DynamicObject.hpp"
+#include <iostream>
+
+// Constructors & Destructor
+DynamicObject::DynamicObject(sf::RenderWindow* window, int x, int y, float width, float height, int speed[2]) : GameObject(window, x, y, width, height)
+{
+	this->speed[0] = speed[0];
+	this->speed[1] = speed[1];
+}
+DynamicObject::DynamicObject(sf::RenderWindow* window, int x, int y, float radius, int speed[2]) : GameObject(window, x, y, radius)
+{
+	this->speed[0] = speed[0];
+	this->speed[1] = speed[1];
+}
+DynamicObject::~DynamicObject()
+{
+	std::cout << "DynamicObject was PURGED\n";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
-#define NOMINMAX
 #include <windows.h>
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include "GameObject.hpp"
+#include "DynamicObject.hpp"
 
 
 int main(int argc, char** argv)
 {
     sf::RenderWindow window(sf::VideoMode(GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)), "SFML");
     sf::Clock clock;
+
+    GameObject testGameObj(&window, 0, 0, 20, 20);
+    int speed[2] = { 1, 1 };
+    DynamicObject testDynaObj(&window, 100, 100, 50, speed);
 
     bool isPlaying = true;
 
