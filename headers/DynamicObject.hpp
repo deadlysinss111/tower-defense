103b001fead328fccd8941f7ed44eb0c8fbfba91
val <vgautier@gaming.tech>
2023-11-23 18:48:40 +0100

    dynamic object speed became vectors

--- headers/DynamicObject.hpp
+++ headers/DynamicObject.hpp
@@ -6,16 +6,19 @@ class DynamicObject : public GameObject
 {
 public:
 	// Constructors & Destructor
-	DynamicObject(sf::RenderWindow* window, int x, int y, float width, float height, int speed[2]);
-	DynamicObject(sf::RenderWindow* window, int x, int y, float radius, int speed[2]);
+	DynamicObject(sf::RenderWindow* window, int x, int y, float width, float height, sf::Vector2f* speed);
+	DynamicObject(sf::RenderWindow* window, int x, int y, float radius, sf::Vector2f* speed);
 	~DynamicObject();
 
 	/*virtual bool collisionsCircleCircle(sf::CircleShape* circleA, sf::CircleShape* circleB, std::array<int, 2> posCircleA, std::array<int, 2> posCircleB);
 	virtual bool collisionsRectRect(sf::RectangleShape* rectA, sf::RectangleShape* rectB, std::array<int, 2> posRectA, std::array<int, 2> posRectB);
 	virtual bool collisionsCirlceRect(sf::CircleShape* circle, sf::RectangleShape* rect, std::array<int, 2> posCircle, std::array<int, 2> posRect);*/
-	void move(sf::Vector2f* vect, float deltaT);
+	
+	void setSpeed(float x, float y);
+	void update(float deltaT);
 
 protected:
 	// Extra attrbiutes of DynamicObject
-	int speed[2];
+	void move(float deltaT);
+	sf::Vector2f speed;
 };
