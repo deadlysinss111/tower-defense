b903ad35815bd0bef51443d2693ce2cd431e3c53
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2023-11-23 16:09:03 +0100

    Update Monster.cpp

--- src/game objects/Monster.cpp
+++ src/game objects/Monster.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 #include <iostream>
 
-namespace std
-{
+using namespace std;
+
 	map<string, vector<int>> Monster::MonsterChoice = { {"soldier", { 75, 8, 3, 3 }},{"tanker", { 100, 10, 2, 5 }} };
 
 	Monster::Monster(sf::RenderWindow* window, int x, int y, int r, string type) : Ennemies::Ennemies(window, x, y, r, 0, 0, 0, 0)
@@ -37,5 +37,5 @@ namespace std
 		this->type = type;
 	}
 
-}
+
 
