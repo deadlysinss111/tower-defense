ffb9acf44d8c0847db9c021a14388a65a299bd5d
Ludovic28 <lguglielmini@gaming.tech>
2023-11-23 17:38:47 +0100

    correction Ennemy

--- src/game objects/Monster.cpp
+++ src/game objects/Monster.cpp
@@ -3,39 +3,39 @@
 #include <vector>
 #include <iostream>
 
-namespace std
+using namespace std;
+
+map<string, vector<int>> Monster::MonsterChoice = { {"soldier", { 75, 8, 3, 3 }},{"tanker", { 100, 10, 2, 5 }} };
+
+Monster::Monster(sf::RenderWindow* window, int x, int y, int r, string type) : Ennemies::Ennemies(window, x, y, r, 0, 0, 0, 0)
 {
-	map<string, vector<int>> Monster::MonsterChoice = { {"soldier", { 75, 8, 3, 3 }},{"tanker", { 100, 10, 2, 5 }} };
+	this->type = type;
 
-	Monster::Monster(sf::RenderWindow* window, int x, int y, int r, string type) : Ennemies::Ennemies(window, x, y, r, 0, 0, 0, 0)
-	{
-		this->type = type;
+	auto found = this->MonsterChoice.find(type);
 
-		auto found = this->MonsterChoice.find(type);
+	if (found != MonsterChoice.end()) {
 
-		if (found != MonsterChoice.end()) {
+		vector<int> values = found->second;
 
-			vector<int> values = found->second;
-
-			Ennemies::setHealth(values[0]);
-			Ennemies::setAttack(values[1]);
-			Ennemies::setSpeed(values[2]);
-			Ennemies::setGoldValue(values[3]);
-		}
-		else {
-			cerr << "Erreur : Type de monstre inconnu" << endl;
-		}
+		Ennemies::setHealth(values[0]);
+		Ennemies::setAttack(values[1]);
+		Ennemies::setSpeed(values[2]);
+		Ennemies::setGoldValue(values[3]);
 	}
-
-	string Monster::getType()
-	{
-		return this->type;
+	else {
+		cerr << "Erreur : Type de monstre inconnu" << endl;
 	}
-
-	void Monster::setType(string type)
-	{
-		this->type = type;
-	}
-
 }
+
+string Monster::getType()
+{
+	return this->type;
+}
+
+void Monster::setType(string type)
+{
+	this->type = type;
+}
+
+
 
